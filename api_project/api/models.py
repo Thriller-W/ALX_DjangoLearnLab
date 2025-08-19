4eb74e698c1d7afbd508fbfb2efbb399db6e7e17
Thriller-W <githinjimurage24@gmail.com>
2025-08-19 15:19:40 +0300

    API Endpoint with Django Rest Framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-
+    published_date = models.DateField(blank=True, null=True)
+    isbn = models.CharField(max_length=13, blank=True)
+    pages = models.PositiveIntegerField(blank=True, null=True)
+    
     def __str__(self):
         return f"{self.title} by {self.author}"
 
