2cb281b4d64288038117c15f2271a435e71eb67f
Thriller-W <githinjimurage24@gmail.com>
2025-08-21 16:41:39 +0300

    Building Custom Views and Generic Views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),                  # /api/books/
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),     # /api/books/1/
     path("books/create/", BookCreateView.as_view(), name="book-create"),       # /api/books/create/
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),   # /api/books/1/update/
-    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
+    path("books/update/", BookUpdateView.as_view(), name="book-update"),   # /api/books/1/update/
+    path("books/delete/", BookDeleteView.as_view(), name="book-delete"),
     path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update-pk"),
     path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete-pk"),
 ]
